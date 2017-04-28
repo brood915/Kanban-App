01e055406f4e96f4ac9dd5d3aa957da47bc4a398
k <brood915@gmail.com>
2017-04-28 17:37:29 -0400

    added rendering method to each taskbox

--- src/components/CompletedBox.js
+++ src/components/CompletedBox.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Tasks from './Tasks';
+import { Button } from 'react-bootstrap';
 
 function CompletedBox(props) {
 	const border = {borderLeft: props.border}
     return (
     	<div className="taskBox" style = {border}>
-    		{props.children}
+	    	{props.children}
+	    	{props.tasks
+	    	.filter((each)=>(each.stage === 'completed'))
+	    	.map((each, index)=>(
+	    	<div  className="tasks" key={index}>
+		    	<Tasks tasks={each}/>
+		    	<Button bsStyle='danger'>Remove</Button>
+		    </div>))}
     	</div>
     );
 }
--- src/components/InProgressBox.js
+++ src/components/InProgressBox.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import Tasks from './Tasks';
+import { Button } from 'react-bootstrap';
 
 function InProgressBox(props) {
 	const border = {borderLeft: props.border}
     return (
     	<div className="taskBox" style = {border}>
-    		{props.children}
+	    	{props.children}
+	    	{props.tasks
+	    	.filter((each)=>(each.stage === 'in-progress'))
+	    	.map((each, index)=>(
+	    	<div  className="tasks" key={index}>
+		    	<Tasks tasks={each}/>
+		    	<Button bsStyle='primary'>Finish</Button>
+		    </div>))}
     	</div>
     );
 }
