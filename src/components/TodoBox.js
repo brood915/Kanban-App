31552643b781019977ea4035724ed4eab3466827
k <brood915@gmail.com>
2017-05-01 17:19:00 -0400

    passed dispatch function to ToDoBox

--- src/components/TodoBox.js
+++ src/components/TodoBox.js
@@ -3,8 +3,14 @@ import PropTypes from 'prop-types';
 import Tasks from './Tasks';
 import { Button } from 'react-bootstrap';
 
+
 function TodoBox(props) {
-	const border = {borderLeft: props.border}
+    function handleOnClick(e) {
+        let index = e.target.getAttribute('id');
+        props.startTask(index);
+        console.log(index);
+    }		
+	const border = {borderLeft: props.border};
     return (
     	<div className="taskBox" style = {border}>
 	    	{props.children}
@@ -13,7 +19,7 @@ function TodoBox(props) {
 	    	.map((each, index)=>(
 	    	<div  className="tasks" key={index}>
 		    	<Tasks tasks={each}/>
-		    	<Button bsStyle='primary'>Start</Button>
+		    	<Button id = {index} onClick = {handleOnClick} bsStyle='primary'>Start</Button>
 		    </div>))}
     	</div>
     );
@@ -23,7 +29,8 @@ function TodoBox(props) {
 
 
 TodoBox.propTypes = {
-
+	 startTask: PropTypes.func.isRequired,
+	 tasks: PropTypes.array.isRequired
 }
 
 export default TodoBox;
